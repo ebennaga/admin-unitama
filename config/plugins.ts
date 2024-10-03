92ab8589f2197670a235d515ad60b50628494588
Eben <freddy.eben@gmail.com>
2024-10-03 15:17:16 +0700

    fix 404

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_DUPsclxMaEsQFjPF0Cb3XzH9nm5z/z5ocpy69GG",
+      apiToken: "ScJsHm3bSUhcwhwOxYxBhuyA",
+      appFilter: "admin-unitama",
+      teamFilter: "no1",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
